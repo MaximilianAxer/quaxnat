787bde8b356fc02421943e3859cf50ac1f7e00b5
Robert Schlicht <robert.schlicht@tu-dresden.de>
2023-06-30 11:50:31 +0200

    Added interface for formulae (...~... expressions) to quax.

--- R/optN.R
+++ R/optN.R
@@ -52,20 +52,42 @@ SN <- function(x, y, tau, fun, w, par, Nmax = 1000000) { # Search for the minimu
 #' @param y Observed regeneration density of the inventory plot.
 #' @param tau Numeric between 0 and 1. Specifies the quantile used for the quantile regression. {0;1}
 #' @param fun Function assumed for the quantile regression of the regeneration potential. Values allowed are: "exponential", "weibull", "gamma", "Clark2dt", "lognormal", "half-normal". The default is to fit an lognormal model.
-#' @param w Numeric vector of weights of the observations in the estimation procedure.
+#' @param weights Numeric vector of weights of the observations in the estimation procedure.
 #' @param par Numeric vector of initial values for the parameters to be optimized over, exluding the first parameter `N`.
 #' @param ... Further arguments passed to `optim`.
+#' @param subset, weights, na.action, offset For the formula interface: Further arguments passed to `model.frame` (along with `weights`).
 #' @details The function return a list including the estimated parameters for the quantile regression for the specific distribution function.
 #'
 #' @return The estimated function, including an attribute `o` containing the results of `optim`.
 
-quax <-  function(x, y, tau, fun=lognormal, w=1, par=c(a=8, b=1), ...) {
-  o <- optim(par, SN, x = x, y = y, tau = tau, fun = fun, w = w, ...)
-  oN <- optN(x=x, y=y, tau=tau, fun=fun, w=w, par=o$par, Nmax = 1000000)
+quax <- function(...) UseMethod("quax")
+
+quax.default <- function(x, y, tau, fun=lognormal, weights=1, par=c(a=8, b=1), ...) {
+  o <- optim(par, SN, x = x, y = y, tau = tau, fun = fun, w = weights, ...)
+  oN <- optN(x=x, y=y, tau=tau, fun=fun, w=weights, par=o$par, Nmax = 1000000)
   formals(fun)$par <- c(o$par, N=oN$minimum)
   attr(fun,"o") <- o
   fun
 }
+
+quax.formula <- function(formula, data, tau, fun=lognormal,
+    subset, weights, na.action, offset, par=c(a=8, b=1), ...) {
+  cl <- match.call(expand.dots=FALSE)
+  cl <- cl[c(1L, match(c("formula","data","subset","weights",
+    "na.action","offset"), names(cl), 0L))]
+  cl[[1L]] <- quote(stats::model.frame)
+  cl$formula[[3L]] <- substitute(0+x, list(x=formula[[3L]]))
+  mf <- eval.parent(cl)
+  x <- model.matrix(attr(mf,"terms"), mf)
+  y <- mf[[1L]]
+  if (ncol(x)!=1L || !is.vector(y))
+    stop("both sides of ~ must be numeric vectors")
+  o <- model.offset(mf)
+  w <- model.weights(mf)
+  quax.default(x=as.vector(x), y=if (is.null(o)) y else y-o,
+    tau=tau, fun=fun, weights=if (is.null(w)) 1 else w, par=par, ...)
+}
+
 
 #the function assumed for the dispersal distance distribution.
 ###        Values allowed are: "exponential", "weibull", "gamma", "pearson",
