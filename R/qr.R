bbc6f7c744b6f906c640cf887b3313290391db95
Robert Schlicht <robert.schlicht@tu-dresden.de>
2023-07-17 13:29:17 +0200

    Worked on function descriptions.

--- R/qr.R
+++ R/qr.R
@@ -22,24 +22,14 @@
 #' which corresponds to a probability density of the distance given by
 #' \deqn{p(r)={1 \over r\sqrt{2\pi \sigma ^{2}}}
 #'   e^{-{1 \over 2\sigma ^{2}}(\log (r/a))^{2}},}
-#' where \eqn{d} is the spatial dimension and \eqn{\left\|{\,}\right\|} 
-#' denotes the Euclidean norm; see Greene and Johnson (1989), Stoyan and 
-#' Wagner (2001) for the planar case. Thus, the distance is assumed to have 
-#' the log-normal distribution such that the log-distance has a normal 
-#' distribution with mean \eqn{a} and variance \eqn{\sigma^2}. Note that 
-#' \eqn{\log k(x)} is a quadratic function of \eqn{\left\|{x}\right\|} with 
-#' a maximum at \eqn{\log a-d\sigma^2}.
-#'
-#' @details The spatial dispersal density, representing the probability
-#' density function, divided by \eqn{2\pi x}, of the distance of a seed from
-#' its source, is here given by
-#' \deqn{f(x) = \frac{1}{2\pi x^2 \sqrt{2\pi\sigma^2}}
-#'   e^{-\frac{1}{2\sigma^2}(\log(x/a))^2},}
-#' see Greene and Johnson (1989), Stoyan and Wagner (2001). Thus, the
-#' distance is assumed to have the log-normal distribution such that the
-#' log-distance has a normal distribution with mean \eqn{a} and variance
-#' \eqn{\sigma^2}. Note that \eqn{\log f(x)} is a quadratic function of
-#' \eqn{\log x} with a maximum at \eqn{\log a-2\sigma^2}.
+#' where \eqn{d} is the spatial dimension, \eqn{\left\|{\,}\right\|} 
+#' denotes the Euclidean norm and the first normalizing constant involves the 
+#' \link[base:beta]{gamma} function; see Greene and Johnson (1989), Stoyan 
+#' and Wagner (2001) for the planar case. Thus, the distance is assumed to 
+#' have the \link[stats:Lognormal]{log-normal distribution} such that the 
+#' log-distance has a normal distribution with mean \eqn{a} and variance 
+#' \eqn{\sigma^2}. Note that \eqn{\log k(x)} is a quadratic function of 
+#' \eqn{\left\|{x}\right\|} with a maximum at \eqn{\log a-d\sigma^2}.
 #'
 #' Particularly suitable if the maximum regeneration density is not
 #' directly at the seed source (e.g. Janzen-Conell effect)(Nathan et al. 
@@ -74,7 +64,7 @@ lognormal <- function(x, par) {
 #' `Clark2dt` computes the value of the dispersal function from Clark et al.
 #' (1999) multiplied by \eqn{N}.
 #'
-#' @return Numeric vector of function values multiplied by \eqn{N}.
+#' @return Numeric vector of function values of multiplied by \eqn{N}.
 #'
 #' @param par Numeric vector with three elements representing the
 #' log-transformed parameters \eqn{a} and \eqn{p} and the scaling \eqn{N}.
@@ -87,20 +77,18 @@ lognormal <- function(x, par) {
 #' which corresponds to a probability density of the distance given by
 #' \deqn{p(r)={2 \over a^{d}\Beta (d/2,p)}r^{d-1}
 #'   (1+{r^{2} \over a^{2}})^{-(p+d/2)},}
-#' where \eqn{d} is the spatial dimension and \eqn{\left\|{\,}\right\|} 
-#' denotes the Euclidean norm; see Clark et al. (1999) and Austerlitz et al. 
-#' (2004) for the planar case (with parameterizations \eqn{a=\sqrt{u}} and 
-#' \eqn{p=b-d/2}, respectively). This represents a mixture of Gaussian 
-#' densities that exhibits heavier tails. It has its maximum at zero.
+#' where \eqn{d} is the spatial dimension, \eqn{\left\|{\,}\right\|} 
+#' denotes the Euclidean norm and the normalizing constants involve the 
+#' \link[base:beta]{beta} and \link[base:beta]{gamma} functions; see Clark 
+#' et al. (1999) and Austerlitz et al. (2004) for the planar case (with 
+#' parameterizations \eqn{a=\sqrt{u}} and \eqn{p=b-d/2}, respectively). This 
+#' means the squared distance is \eqn{da^{2} \over 2p} times a random 
+#' variable having an \link[stats:FDist]{F distribution} with \eqn{d} and 
+#' \eqn{2p} degrees of freedom.
 #'
-#' @details The spatial dispersal density, representing the probability
-#' density function, divided by \eqn{2\pi x}, of the distance of a seed from
-#' its source, is here given by
-#' \deqn{f(x) = \frac{p}{\pi a^2 (1+(x/a)^2)^{p+1}},}
-#' see Clark et al. (1999) and Austerlitz et al. (2004) (with
-#' parameterizations \eqn{a=\sqrt{u}} and \eqn{p=b-1}, respectively). This
-#' represents a mixture of Gaussian densities that exhibits heavier tails. It
-#' has its maximum at zero.
+#' This dispersal kernel represents a mixture of Gaussian densities (with 
+#' inverse variance following a gamma distribution with shape parameter 
+#' \eqn{p}) that exhibits heavier tails. It has its maximum at zero.
 #'
 #' @references
 #' Clark, J.S., Silman, M., Kern, R., Macklin, E., HilleRisLambers, J.
@@ -148,21 +136,18 @@ Clark2dt <- function(x, par){
 #'    e^{-(\left\|{x}\right\|/a)^{b}},}
 #' which corresponds to a probability density of the distance given by
 #' \deqn{p(r)={b \over a^{d}\Gamma (d/b)}r^{d-1}e^{-(r/a)^{b}},}
-#' where \eqn{d} is the spatial dimension and \eqn{\left\|{\,}\right\|} 
-#' denotes the Euclidean norm; see Bateman (1947), Clark et al. (1998), 
+#' where \eqn{d} is the spatial dimension, \eqn{\left\|{\,}\right\|} 
+#' denotes the Euclidean norm and the normalizing constants involve the 
+#' \link[base:beta]{gamma} function; see Bateman (1947), Clark et al. (1998), 
 #' Austerlitz et al. (2004), Nathan et al. (2012) for the planar case.
 #' 
-#' @details The spatial dispersal density, representing the probability
-#' density function, divided by \eqn{2\pi x}, of the distance of a seed from
-#' its source, is here given by
-#' \deqn{f(x) = \frac{b}{2\pi a^2\Gamma(2/b)} e^{-(x/a)^b},}
-#' see Bateman (1947), Clark et al. (1998), Austerlitz et al. (2004). This
-#' function has its maximum at zero and represents a rather flexible family
-#' of distributions including the classical bivariate Gaussian kernels, the
-#' kernels based on an exponential distribution of distances, and, for
-#' \eqn{b<1}, fat-tailed distributions. For \eqn{b>1}, it shows thin-tailed distributions.
-#' It has consequently been applied in a number of theoretical studies that
-#' address dispersal (Ribbens et al. 1994; Bullock et al. 2017).
+#' This function has its maximum at zero and represents a rather flexible 
+#' family of distributions including the classical bivariate Gaussian 
+#' kernels, the kernels based on an exponential distribution of distances, 
+#' and, for \eqn{b<1}, fat-tailed distributions. For \eqn{b>1}, it shows 
+#' thin-tailed distributions. It has consequently been applied in a number of 
+#' theoretical studies that address dispersal (Ribbens et al. 1994; 
+#' Bullock et al. 2017).
 #'
 #' @references
 #' Bateman, A. (1947). Contamination in seed crops: III. relation with
@@ -234,16 +219,14 @@ quax_exponential.power <- function(
 #'   e^{-(\left\|{x}\right\|/a)^{b}},}
 #' which corresponds to a probability density of the distance given by
 #' \deqn{p(r)={b \over a^{b}}r^{b-1}e^{-(r/a)^{b}},}
-#' where \eqn{d} is the spatial dimension and \eqn{\left\|{\,}\right\|} 
-#' denotes the Euclidean norm; see Tufto et al. (1997) for the planar case.
-#' Thus, the distance is assumed to have the Weibull distribution with scale 
-#' parameter \eqn{a} and shape parameter \eqn{b}.
+#' where \eqn{d} is the spatial dimension, \eqn{\left\|{\,}\right\|} 
+#' denotes the Euclidean norm and the normalizing constants involve the 
+#' \link[base:beta]{gamma} function; see Tufto et al. (1997) for the planar 
+#' case. Thus, the distance is assumed to have the 
+#' \link[stats:Weibull]{Weibull distribution} with scale parameter \eqn{a} 
+#' and shape parameter \eqn{b}. Equivalently, the \eqn{b}th power of the 
+#' distance has an exponential distribution with scale parameter \eqn{a}.
 #' 
-#' @details The spatial dispersal density, representing the probability
-#' density function, divided by \eqn{2\pi x}, of the distance of a seed from
-#' its source, is here given by
-#' \deqn{f(x) = \frac{a^{-b}b}{2\pi} (\frac{x}{a})^{b-2} e^{-(x/a)^b},}
-#' see Tufto et al. (1997), Austerlitz et al. (2004).
 #' The distribution is fat-tailed when \eqn{b<1} and
 #' thin-tailed otherwise (Nathan et al. 2012).
 #' For \eqn{b>1}, the mode of the function is at \eqn{x>1}. In this way, the function approaches the normal distribution.
@@ -290,16 +273,15 @@ Weibull <- function(x, par) {
 #'   (1+{\left\|{x}\right\| \over a})^{-b},}
 #' which corresponds to a probability density of the distance given by
 #' \deqn{p(x)={1 \over a^{d}\Beta(d,b-d)}r^{d-1}(1+{r \over a})^{-b},}
-#' where \eqn{d} is the spatial dimension and \eqn{\left\|{\,}\right\|} 
-#' denotes the Euclidean norm; see Nathan et al. (2012) for the planar case
-#' (DOUBLE CHECK EQUATIONS).
-#'
-#' @details The spatial dispersal density, representing the probability
-#' density function, divided by \eqn{2\pi x}, of the distance of a seed from
-#' its source, is here given by
-#' \deqn{f(x) = \frac{(b-2)(b-1)}{2\pi a^2} (1+\frac{x}{a})^{-b},}
-#' see Nathan et al. (2012).
-#' (CHANGE THE FOLLOWING, SHOULD BE OUR OWN CHARACTERIZATION:) Austerlitz 
+#' where \eqn{d} is the spatial dimension, \eqn{\left\|{\,}\right\|} 
+#' denotes the Euclidean norm and the normalizing constants involve the 
+#' \link[base:beta]{beta} and \link[base:beta]{gamma} functions; see Nathan 
+#' et al. (2012) for the planar case. This means the distance is 
+#' \eqn{da \over b-d} times a random variable having an 
+#' \link[stats:FDist]{F distribution} with \eqn{2d} and \eqn{2(b-d)} degrees 
+#' of freedom.
+
+#' Austerlitz 
 #' et al. (2004) characterize it as follows: The geometric and 2dt families 
 #' “will behave quite differently from the exponential and Weibull 
 #' distributions. They show a fat tail, whatever the value of the shape 
